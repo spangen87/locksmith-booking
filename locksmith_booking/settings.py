a22a6607e246aa8e8db679eec6927e824751aaaa
spangen87 <rikard.spangmyr@bestlas.se>
2022-10-03 12:41:55 +0000

    Fix typo in ALLOWED_HOSTS

--- locksmith_booking/settings.py
+++ locksmith_booking/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['locksmith-booking.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['locksmith-booking.herokuapp.com', 'localhost']
 
 
 # Application definition
