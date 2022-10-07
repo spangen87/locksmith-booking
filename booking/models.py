1560d6d162205070121f0de8ebae3b7ed65a5d83
spangen87 <rikard.spangmyr@bestlas.se>
2022-10-07 11:56:41 +0000

    Add model for PlaceBooking and set up superuser for django admin

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PlaceBooking(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.TextField()
+    email = models.EmailField()
+    phone = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    approved = models.BooleanField(default=False)
+    date_for_visit = models.DateField()
+    time_for_visit = models.TimeField()
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.first_name
+    
