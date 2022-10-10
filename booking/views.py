4e587907522e77acd90825f65786fc769fc674e4
spangen87 <rikard.spangmyr@bestlas.se>
2022-10-10 12:48:11 +0000

    Fix issue with form so it posted to the database

--- booking/views.py
+++ booking/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import PlaceBooking
 from .forms import BookingForm
 
 # Create your views here.
 def get_index(request):
-    return render(request, 'booking/index.html', {'form': BookingForm()})
+    return render(request, 'booking/index.html')
 
 
 def place_booking(request):
-    return render(request, 'booking/place_booking.html', {'form': BookingForm()})
-    form = BookingForm(data=request.POST)
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(request, 'booking/place_booking.html')
-    
+            return redirect('place_booking')
+    form = BookingForm()
+    context = {'form': form}
+    return render(request, 'booking/place_booking.html', context)        
