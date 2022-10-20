ee3395ac0856d8c3d43820af4ce652081e7a48b3
spangen87 <rikard.spangmyr@bestlas.se>
2022-10-20 09:31:33 +0000

    Rebuild views that should send email when changed and add login_required decorator to views that need user to be logged in

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
 from .models import PlaceBooking, Review, User
 from .forms import BookingForm, ReviewForm, EditBooking
 
@@ -17,6 +18,7 @@ def about(request):
     return render(request, 'booking/about.html')
 
 
+@login_required
 def place_booking(request):
     if request.method == 'POST':
         booking = PlaceBooking(user=request.user)
@@ -30,6 +32,7 @@ def place_booking(request):
     return render(request, 'booking/place_booking.html', {'form': form})
 
 
+@login_required
 def view_booking(request):
     if request.user.is_staff:
         bookings = PlaceBooking.objects.all()
@@ -41,42 +44,68 @@ def view_booking(request):
     return render(request, 'booking/my_account.html', context)
 
 
+@login_required
 def delete_booking(request, booking_id):
-    booking = get_object_or_404(PlaceBooking, id=booking_id)
-    booking.delete()
-    email = PlaceBooking.email
-    messages.success(request, 'Booking deleted successfully.')
-    subject = 'Your booking'
-    message = f'Hi, your booking has been cancelled.'
-    email_from = 'bestlasbooking@gmail.com'
-    recipient_list = [email, ]
-    send_mail(subject, message, email_from, recipient_list)
-    return redirect('my_account')
+    email_to = None
+    try:
+        booking = get_object_or_404(PlaceBooking, id=booking_id)
+        email_to = booking.email
+        subject = 'Your booking'
+        message = f'Hi {booking.first_name}, your booking has been cancelled.'
+        email_from = 'bestlasbooking@gmail.com'
+        recipient_list = [email_to, ]
+        send_mail(subject, message, email_from, recipient_list)
+        booking.delete()
+        messages.success(request, 'Booking deleted successfully.')
+        return redirect('my_account')
+    except Http404 as err:
+        messages.error(request, 'Oops, booking not found.')
+        return redirect('my_account')
 
 
+@login_required
 def edit_booking(request, booking_id):
-    booking = get_object_or_404(PlaceBooking, id=booking_id)
-    if request.method == 'POST':
-        form = EditBooking(request.POST, instance=booking)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Updated successfully!')
-            return redirect('my_account')
-    form = EditBooking(instance=booking)
-    context = {
-            'form': form
-        }
-    return render(request, 'booking/edit_booking.html', context)
+    email_to = None
+    try:
+        booking = get_object_or_404(PlaceBooking, id=booking_id)
+        if request.method == 'POST':
+            form = EditBooking(request.POST, instance=booking)
+            if form.is_valid():
+                form.save()
+                email_to = booking.email
+                subject = 'Your booking'
+                message = f'Hi {booking.first_name}, your booking has been updated. Log in to see details.'
+                email_from = 'bestlasbooking@gmail.com'
+                recipient_list = [email_to, ]
+                send_mail(subject, message, email_from, recipient_list)
+                messages.success(request, 'Updated successfully!')
+                return redirect('my_account')
+        form = EditBooking(instance=booking)
+        context = {
+                'form': form
+            }
+        return render(request, 'booking/edit_booking.html', context)
+    except Http404 as err:
+        messages.error(request, 'Oops, booking not found.')
+        return redirect('my_account')
 
 
+@login_required
 def approve_booking(request, booking_id):
     booking = get_object_or_404(PlaceBooking, id=booking_id)
     booking.approved = not booking.approved
     booking.save()
-    send_mail('Test', 'Din bokning är godkänd', 'bestlasbooking@gmail.com', [PlaceBooking.email])
+    email_to = booking.email
+    subject = 'Your booking'
+    message = f'Hi {booking.first_name}, your booking has been updated. Log in to see details.'
+    email_from = 'bestlasbooking@gmail.com'
+    recipient_list = [email_to, ]
+    send_mail(subject, message, email_from, recipient_list)
+    messages.success(request, 'Booking Updated successfully!')
     return redirect('my_account')
 
 
+@login_required
 def place_review(request):
     if request.method == 'POST':
         review = Review(user=request.user)
